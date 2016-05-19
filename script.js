25b08f703595ea6e2cf7bc3ff38891bb07fae2e1
mmish321 <mm321whatsup@gmail.com>
2016-05-19 11:01:18 -0600

    I think I fixed it
    Alright, so I finished the first loop to make sure that the special cases like Evee work out and the second loop does the same exact thing but it add the second evolve_to.evolution_details thing.
    I'll try and work on it more tonight

--- script.js
+++ script.js
@@ -8,71 +8,90 @@ $(document).ready(function() {
   var level = 36;
   var canEvolve = false;
 
-	$.getJSON('http://pokeapi.co/api/v2/pokemon-species/' + id)
+$.getJSON('http://pokeapi.co/api/v2/pokemon-species/' + id)
+	.done(function(myArr) {
+	url = myArr.evolution_chain.url;
+	$.getJSON(url)
 		.done(function(myArr) {
-			url = myArr.evolution_chain.url;
-			$.getJSON(url)
-				.done(function(myArr) {
-				if (myArr.chain.species.name == id) {
-					canEvolve = true;+
-					console.log(myArr.chain.evolves_to[0].evolution_details);
-				}
-				for (i = 0; i < myArr.chain.evolves_to.length; i++) {
-					if (myArr.chain.evolves_to[i].species.name == id && myArr.chain.evolves_to[i].evolves_to.length > 0) {
-					  canEvolve = true;
-					    if (canEvolve) {
-					    	console.log(myArr);
-					    	console.log(myArr.chain.evolves_to[i].evolves_to[i].evolution_details);
-					    }
+		if (myArr.chain.species.name == id) {
+			canEvolve = true;+
+	                if (canEvolve) {
+	                  	for (i = 0; i < myArr.chain.evolves_to.length; i++) {
+					if(myArr.chain.evolves_to[i].evolution_details.gender != null){
+						if(myArr.chain.evolves_to[i].evolution_details.gender != gender){
+							console.log("I'm sorry your pokemon is the wrong gender!");
+						}
 					}
-					// if (myArr.chain.evolves_to[i].evolves_to.length == 0 && myArr.chain.evolves_to[i].species.name == id) {
-					// 	canEvolve = false;
-					// }
-					// if (myArr.chain.evolves_to[i].evolves_to.length > 0) {
-					// 	for (j = 0; j < myArr.chain.evolves_to[i].evolves_to.length; j++) {
-					// 		if (myArr.chain.evolves_to[i].evolves_to[j].species.name == id && myArr.chain.evolves_to[i].evolves_to[j].evolves_to.length == 0) {
-					// 			canEvolve = false;
-					// 		}
-					// 	}
-					// }
+					if(myArr.chain.evolves_to[i].evolution_details.min_level != null){
+						if(myArr.chain.evolves_to[i].evolution_details.min_level < level){
+							console.log("I'm sorry your level is not enough");
+							console.log("You need " + myArr.chain.evolves_to[i].evolution_details.min_level - level + ' more');
+						}
+					}
+					if(myArr.chain.evolves_to[i].evolution_details.min_beauty != beauty){
+						if(myArr.chain.evolves_to[i].evolution_details.beauty < beauty){
+							console.log("I'm sorry your beauty is not enough");
+							console.log("You need " + myArr.chain.evolves_to[i].evolution_details.min_beauty - beauty + ' more');
+						}
+					}
+					if(myArr.chain.evolves_to[i].evolution_details.min_happiness != null){
+						if(myArr.chain.evolves_to[i].evolution_details.min_happiness < happiness){
+							console.log("I'm sorry your happiness is not enough");
+							console.log("You need " + myArr.chain.evolves_to[i].evolution_details.min_happiness - happiness + ' more');
+						}
+					}
+					if(myArr.chain.evolves_to[i].evolution_details.item != null){
+						if(myArr.chain.evolves_to[i].evolution_details.item != item){
+							console.log("You need " + myArr.chain.evolves_to[i].evolution_details.item + " to evolve" );
+						}
+					}
+				 console.log("You can evolve to " myArr.chain.evolves_to[i].species.name);
+			        }
+			}
+		
+		 }
+	
+	for (i = 0; i < myArr.chain.evolves_to.length; i++) {
+		if (myArr.chain.evolves_to[i].species.name == id && myArr.chain.evolves_to[i].evolves_to.length > 0) {
+		  canEvolve = true;
+		    if (canEvolve) {
+	    		for (j = 0; j < myArr.chain.evolves_to[i].evolves_to.length; i++) {
+				if(myArr.chain.evolves_to[i].evolves_to[j].evolution_details.gender != null){
+					if(myArr.chain.evolves_to[i].evolves_to[j].evolution_details.gender != gender){
+						console.log("I'm sorry your pokemon is the wrong gender!");
+					}
 				}
-			// if (canEvolve) {
-			// 	console.log(myArr);
-			// 	for (i = 0; i < myArr.chain.evolves_to.length; i++) {
-			// 		console.log(i);
-			// 			for (j = 0; j < myArr.chain.evolves_to[i].evolves_to.length; j++) {
-			// 				console.log(myArr.chain.evolves_to[i].evolution_details);
-			// 				if(myArr.chain.evolves_to[i].evolution_details.gender != null){
-			// 					if(myArr.chain.evolves_to[i].evolution_details.gender != gender){
-			// 						canEvolve = false;
-			// 					}
-			// 				}
-			// 				if(myArr.chain.evolves_to[i].evolution_details.min_level != null){
-			// 					if(myArr.chain.evolves_to[i].evolution_details.min_level < level){
-			// 						canEvolve = false;
-			// 					}
-			// 				}
-			// 				if(myArr.chain.evolves_to[i].evolution_details.min_beauty != beauty){
-			// 					if(myArr.chain.evolves_to[i].evolution_details.beauty < beauty){
-			// 						canEvolve = false;
-			// 					}
-			// 				}
-			// 				if(myArr.chain.evolves_to[i].evolution_details.min_happiness != null){
-			// 					if(myArr.chain.evolves_to[i].evolution_details.min_happiness < happiness){
-			// 						canEvolve = false;
-			// 					}
-			// 				}
-			// 				if(myArr.chain.evolves_to[i].evolution_details.item != null){
-			// 					if(myArr.chain.evolves_to[i].evolution_details.item != item){
-			// 						canEvolve = false;
-			// 					}
-			// 				}
-			// 				if(canEvolve){
-			// 					console.log("You can evolve into " + myArr.chain.evolves_to[i].species.name);
-			// 				}
-			// 			 }
-			// 		}
-			// }
+				if(myArr.chain.evolves_to[i].evolves_to[j].evolution_details.min_level != null){
+					if(myArr.chain.evolves_to[i].evolves_to[j].evolution_details.min_level < level){
+						console.log("I'm sorry your level is not enough");
+						console.log("You need " + myArr.chain.evolves_to[i].evolves_to[j].evolution_details.min_level - level + ' more');
+					}
+				}
+				if(myArr.chain.evolves_to[i].evolves_to[j].evolution_details.min_beauty != beauty){
+					if(myArr.chain.evolves_to[i].evolves_to[j].evolution_details.beauty < beauty){
+						console.log("I'm sorry your beauty is not enough");
+						console.log("You need " + myArr.chain.evolves_to[i].evolves_to[j].evolution_details.min_beauty - beauty + ' more');
+					}
+				}
+				if(myArr.chain.evolves_to[i].evolves_to[j].evolution_details.min_happiness != null){
+					if(myArr.chain.evolves_to[i].evolves_to[j].evolution_details.min_happiness < happiness){
+						console.log("I'm sorry your happiness is not enough");
+						console.log("You need " + myArr.chain.evolves_to[i].evolves_to[j].evolution_details.min_happiness - happiness + ' more');
+					}
+				}
+				if(myArr.chain.evolves_to[i].evolves_to[j].evolution_details.item != null){
+					if(myArr.chain.evolves_to[i].evolves_to[j].evolution_details.item != item){
+						console.log("You need " + myArr.chain.evolves_to[i].evolves_to[j].evolution_details.item + " to evolve" );
+					}
+				}
+			  console.log("You can evolve into " + myArr.chain.evolves_to[i].evolves_to[j].species.name);
+		        }
+		    
+		    }
+		}
+	}
+	
+				
 				});
 		});
 });
